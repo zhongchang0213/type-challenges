d00e80f864318fdeb933c95b32ad5a88b2435054
zhongchang <szzhongchang@ininin.com>
2022-07-01 14:07:13 +0800

    Exclude

--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,11 @@
-type MyExclude<T, U> = any
+type MyExclude<T, U> = T extends U ? never : T
+
+type a = Exclude<string | number | (() => void), Function>
+
+type uni = 'a' | 'b' | 'c'
+type I = MyExclude<uni, 'a' | 'b'>
+
+type MC<T> = T extends 'a' ? 'a' : T
+type D = MC<uni>
+
+// 初步结论： 两个泛型之间继承 涉及到依次分发
