a67772c7fbc737a7191dab5c4f41518525613e17
zhongchang <szzhongchang@ininin.com>
2022-07-01 09:20:59 +0800

    readonly tuple length

--- questions/00018-easy-tuple-length/template.ts
+++ questions/00018-easy-tuple-length/template.ts
@@ -1,1 +1,11 @@
-type Length<T> = any
+type Length<T extends readonly any[]> = T['length']
+
+type myTuple = readonly any[]
+
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type T = typeof tesla
+
+type C = T extends myTuple ? true : false
+
+// tuple  readonly any[] 不可修改的数组
