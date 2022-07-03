3d356c3795547b32d59ea8a092ab793d8b799c8e
chang <zChangWeb@163.com>
2022-07-03 19:54:11 +0800

    array 结构..扩展运算符

--- questions/00533-easy-concat/template.ts
+++ questions/00533-easy-concat/template.ts
@@ -1,1 +1,11 @@
-type Concat<T, U> = any
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U]
+
+// 新增First Tail 获取数据头部和尾部元素
+
+type First<T extends unknown[]> = T extends [infer F, ...infer _r] ? F : never
+
+type Tail<T extends unknown[]> = T extends [...any, infer L] ? L : never
+
+// 数组继承可以用infer 与 ...运算符展开， 其中支持...any来达到去末尾的元素
+
+export {}
